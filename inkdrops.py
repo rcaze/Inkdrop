d00d2a94d480994eb6e9eccd90b9e37e644f6dd4
rcaze <romain.caze@gmai.com>
2019-12-15 19:24:00 +0100

    A working inkdrop rain with multiple drops possible at the same time

--- inkdrops.py
+++ inkdrops.py
@@ -41,7 +41,7 @@ def adjust_spines(ax, spines):
 
 
 rmax = 0.2
-colored = True
+colored = False
 
 if colored:
     fig = plt.figure(figsize=(15,5))
@@ -53,50 +53,50 @@ else:
     adjust_spines(ax, ['']) #Remove the axis
 
 
-def init(tmax=300, colored=False):
+tmax= 300 #If colored is true need to be possible to divide by 3
+ndrops = 10
+drop_life = 50
+rmax = 0.4
+# Need to define the position of all drops
+# Their opacity at a given time and the related diameter
+def init():
     # Generate the random position of the drops
-    xpos = random.rand(tmax)
-    ypos = random.rand(tmax)
-    color = [(0, random.rand(), 0) for i in range(tmax)]
-    if colored:
-        xpos = np.concatenate((random.rand(tmax//3),
-                               random.rand(tmax//3)+1,
-                               random.rand(tmax//3)+2))
-        color = [(random.rand(), 0, 0) for i in range(tmax//3)]
-        color += [(0, random.rand(), 0) for i in range(tmax//3)]
-        color += [(0, 0, random.rand()) for i in range(tmax//3)]
-        color = np.array(color)
-        reorder = [i for i in range(tmax)]
-        random.shuffle(reorder)
-        xpos = xpos[reorder]
-        color = color[reorder]
-    return xpos, ypos, color
+    fall = np.zeros((ndrops, 4, tmax))
+    for i in range(ndrops):
+        fall_t = np.random.randint(tmax)
+        coor = np.random.rand(2)
+        k = 0
+        radius = np.linspace(0.05, rmax, drop_life)
+        opacity = np.linspace(1, 0, drop_life)
+        for j in range(fall_t, min(fall_t + drop_life, tmax)):
+            fall[i,0,j] = opacity[k]
+            fall[i,1,j] = radius[k]
+            fall[i,2,j] = coor[0]
+            fall[i,3,j] = coor[1]
+            k +=1
+    return fall
 
+fall = init()
 
-tmax=300
-xpos, ypos, color = init(tmax, colored)
-
-def ink_rain(t, drop_life=20, rmax=rmax):
+def ink_rain(t):
     """Generate a drawing of diluted inkdrops"""
     plt.cla()
     adjust_spines(ax, ['']) #Remove the axis
-    ax.set_xlim(0-rmax, 3+rmax)
+    ax.set_xlim(0-rmax, 1+rmax)
     ax.set_ylim(0-rmax, 1+rmax)
-    opacity = np.linspace(1, 0, drop_life)
-    radius = np.linspace(0.05, rmax, drop_life)
-    j = 0
-    for i in range(t, max(0, t-drop_life), -1):
-        circle = plt.Circle((xpos[i], ypos[i]),
-                            radius=radius[j],
-                            fc=color[i],
-                            alpha=opacity[j])
-        j += 1
-        ax.add_patch(circle)
+    for i in range(ndrops):
+        if fall[i,0,t] > 0.1:
+            circle = plt.Circle((fall[i, 2, t], fall[i, 3, t]),
+                                radius=fall[i, 1,t],
+                                fc="black",
+                                alpha=fall[i, 0,t])
+            ax.add_patch(circle)
     return ax
 
 if __name__ == "__main__":
     anim = FuncAnimation(fig, ink_rain, init_func=init,
-                         frames=tmax, interval=50)
-    anim.save('ink_rain.avi')
+                         frames=tmax, interval=20)
+    anim.save('Rainy_sunday.avi')
+    #anim.save('spreading_ink.avi')
     plt.show()
 
